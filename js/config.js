b72221af0aeb61a3f46d7ef0a1d15e2e88056598
Jens Hinrichs <jens.hinrichs@gmail.com>
2020-11-23 22:53:17 +0100

    chenge config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_MfLAzSbqq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6s4vtejgqv5ui8is8mm26jpkk4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
